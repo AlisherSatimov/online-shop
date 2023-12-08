b16e5cb5e9980bf8a3e06cfe5f67d7956861cd1a
Alisher Satimov <alisher@kali.alisher>
2023-12-08 09:11:04 +0500

    products added

--- js/product.js
+++ js/product.js
@@ -1,59 +1,89 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  axios.defaults.baseURL = "http://localhost:5050/api/v1";
-  let form = document.querySelector("#form-create");
+    axios.defaults.baseURL = "http://localhost:5050/api/v1/";
 
-  e.preventDefault();
+    let token = localStorage.getItem("token");
+    let cardWrapper = document.querySelector(".card-wrapper");
+    let formProducts = document.querySelector("#form-products");
 
-  let token = localStorage.getItem("token");
+    formProducts.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-  let getProductsResponse = await axios.get("/products", {
-    headers: { authorization: `Bearer ${token}` },
-  });
+        let pName = formProducts[0].value;
+        let pCategory = formProducts[1].value;
+        let pColor = formProducts[2].value;
+        let pExists = formProducts[3].value;
+        let pDescription = formProducts[4].value;
+        let pPrice = formProducts[5].value;
+        let pImage = formProducts[6].value;
 
-  let productsTable = document.querySelector("#products-table");
-  productsTable.innerHTML = " ";
+        console.log(pName);
+        console.log(pCategory);
+        console.log(pColor);
+        console.log(pExists);
+        console.log(pDescription);
+        console.log(pPrice);
+    });
 
-  let products = getProductsResponse.data;
+    let getProducts = await axios.get("/products", {
+        headers: { authorization: `Bearer ${token}` },
+    });
 
-  products.forEach((product) => {
-    let productList = document.createElement("tr");
+    console.log(getProducts.data);
 
-    let productsName = document.createElement("th");
-    if (!product.name) {
-      product.name = "Kiritilmagan";
-    }
-    productsName.innerText = product.name;
-    productList.append(productsName);
+    getProducts.data.forEach((products) => {
+        let card = document.createElement("div");
+        card.classList.add("card", "rounded-3");
+        cardWrapper.append(card);
 
-    let productPrice = document.createElement("td");
-    if (!product.price) {
-      product.price = "Kiritilmagan";
-    }
-    productPrice.innerText = product.price;
-    productList.append(productPrice);
+        let cardHeader = document.createElement("div");
+        cardHeader.classList.add("card-header");
+        card.append(cardHeader);
 
-    let productCategory = document.createElement("td");
-    if (!product.category) {
-      product.category = "Kiritilmagan";
-    }
-    productCategory.innerText = product.category;
-    productList.append(productCategory);
+        let cardImg = document.createElement("img");
+        cardImg.setAttribute("src", products.image);
+        cardHeader.append(cardImg);
 
-    let productColor = document.createElement("td");
-    if (!product.color) {
-      product.color = "Kiritilmagan";
-    }
-    productColor.innerText = product.color;
-    productList.append(color);
-    productsTable.append(productColor);
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body", "d-flex", "flex-column");
+        card.append(cardBody);
 
-    let productDescription = document.createElement("td");
-    if (!product.description) {
-      product.description = "Kiritilmagan";
-    }
-    productDescription.innerText = product.description;
-    productList.append(description);
+        let name = document.createElement("h4");
+        name.innerText = "Name: " + products?.name.uz;
+        cardBody.append(name);
 
-    productsTable.append(productColor);
-  });
+        let category = document.createElement("p");
+        category.innerText = "Category: " + products.category?.uz;
+        cardBody.append(category);
+
+        let color = document.createElement("p");
+        color.innerText = "Color: " + products?.color.uz;
+        cardBody.append(color);
+
+        let isExists = document.createElement("p");
+        isExists.innerText = "Exists: " + products?.exists;
+        cardBody.append(isExists);
+
+        let description = document.createElement("p");
+        description.innerText = "Description: " + products?.description.uz;
+        cardBody.append(description);
+
+        let cardFooter = document.createElement("div");
+        cardFooter.classList.add(
+            "card-footer",
+            "flex-column",
+            "d-flex",
+            "gap-2"
+        );
+        card.append(cardFooter);
+
+        let price = document.createElement("span");
+        price.innerText = "Price: " + "$" + products.price;
+        cardFooter.append(price);
+
+        let btn = document.createElement("button");
+        btn.type = "submit";
+        btn.innerText = "Add to cart";
+        btn.classList.add("btn", "btn-primary");
+        cardFooter.append(btn);
+    });
 });
